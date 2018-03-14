e11f8eeb0738fc009ba92eda22c7c3faa023ca40
nicolas <n.nuiro@gmail.com>
2018-03-14 11:51:19 +0100

    thirdcommit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
 
-const port = process.env.PORT || 3000
+app.use('/', express.static(__dirname + '/front/dist'));
 
 app.get('*', (req,res) => {
-    res.send('test un deux trois')
+    res.sendFile(path.join(__dirname, 'front/dist/index.html'))
 })
 
+const port = process.env.PORT || 3000
 app.listen(port, (err) => {
 
     if(err){
